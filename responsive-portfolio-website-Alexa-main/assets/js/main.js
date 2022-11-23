94b348b69e5abd37e5e860ce9d03311557c45e95
jaynipha <josephjenny490@gmail.com>
2022-11-23 17:24:17 +0100

    media queries for responsive portfolio website

--- responsive-portfolio-website-Alexa-main/assets/js/main.js
+++ responsive-portfolio-website-Alexa-main/assets/js/main.js
@@ -89,7 +89,7 @@ modalCloses.forEach((modalClose) => {
 });
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new swiper('.portfolio-container', {
+let swiperPortfolio = new swiper('.portfolio__container', {
 	cssMode: true,
 	navigation: {
 		nextE1: '.swiper-button-next',
@@ -97,6 +97,7 @@ let swiper = new swiper('.portfolio-container', {
 	},
 	pagination: {
 		el: '.swiper-pagination',
+		clickable: true,
 	},
 
 	mousewheel: true,
@@ -104,11 +105,94 @@ let swiper = new swiper('.portfolio-container', {
 });
 
 /*==================== TESTIMONIAL ====================*/
-
+let swiperTestimonial = new swiper('.testimonial__container', {
+	loop: true,
+	grabCursor: true,
+	spaceBetween: 48,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+		dynamicBullets: true,
+	},
+	breakpoints: {
+		568: {
+			slidesPerView: 2,
+		},
+	},
+});
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const section = document.querySelectorAll('section[id]');
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollActive() {
+	const scrollY = window.pageYOffset;
+	sections.forEach((current) => {
+		const sectionHeight = current.offsetHeight;
+		const sectionTop = current.offsetTop - 50;
+
+		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+			document
+				.querySelector('.nav__menu a[href *=' + sectionId + ']')
+				.classlist.add('active-link');
+		} else {
+			document
+				.querySelector('.nav__menu a[href *=' + sectionId + ']')
+				.classlist.remove('active-link');
+		}
+	});
+}
+window.addEventListener('scroll', scrollActive);
+
+/*===================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
+	const nav = document.getElementById('header');
+	//when the scroll is greater than 200vh, add the scroll-header class to the header tag
+	if (this.scrollY >= 200) nav.classList.add('scroll-header');
+	else nav.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader);
 
 /*==================== SHOW SCROLL UP ====================*/
-
+function scrollUp() {
+	const scrollUp = document.getElementById('scroll-top');
+	//when the scroll is higher than 560vh, add the show-scroll class to the a tag with the scroll
+	if (this.scrollY >= 560) scrollUp.classList.add('show-scroll');
+	else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
 /*==================== DARK LIGHT THEME ====================*/
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'uil-sun';
+
+//previously selected topic(if user selected)
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+//we obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = document.body.classlist.contains(darkTheme)
+	? 'dark'
+	: 'light';
+const getCurrentIcon = themeButton.classlist.contains(iconTheme)
+	? 'uil-moon'
+	: 'uil-sun';
+
+//we validate if the user previously chose a topic
+if (selectedTheme) {
+	//if the validation is fulfilled, we ask what the issue was to know
+	document.body.classlist[selectedTheme === 'dark' ? 'add' : 'remove'](
+		darkTheme
+	);
+	themeButton.classlist[selectedIcon === 'uil-moon' ? 'add' : 'remove'](
+		iconTheme
+	);
+}
+//activate or deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+	//add or remove the dark/icon theme
+	document.body.classList.toggle(darkTheme);
+	themeButton.classList.toggle(iconTheme);
+
+	//we save the theme and the current theme icon that the user chose
+	localStorage.setItem('selected-theme', getCurrentTheme());
+	localStorage.setItem('selected-icon', getCurrentIcon());
+});
